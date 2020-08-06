88293ab5a9be8604541a0d9cb0a04d9265100247
DinoAndCat <jokerdj@126.com>
2020-08-06 23:23:48 +0800

    # 修改player _live 属性为Animation

--- lib/component/player.dart
+++ lib/component/player.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flame/animation.dart';
-import 'package:flame/components/animation_component.dart';
 import 'package:flame/components/component.dart';
 import 'package:flame/components/mixins/has_game_ref.dart';
 import 'package:flame/sprite.dart';
@@ -10,7 +9,8 @@ import 'package:flame/sprite.dart';
 import '../biu_biu_game.dart';
 
 class Player extends PositionComponent with HasGameRef<BiuBiuGame> {
-  AnimationComponent _live;
+  Paint overridePaint;
+  Animation _live;
 
   //生命值
   int life = 1;
@@ -22,20 +22,14 @@ class Player extends PositionComponent with HasGameRef<BiuBiuGame> {
     //player图片宽高为102*126 设置player 宽度为 1 tileSize
     width = gameRef.tileSize;
     height = 126 / 102 * gameRef.tileSize;
-    //设置player
-    _live = AnimationComponent(
-      width,
-      height,
-      Animation.spriteList(
-        [
-          Sprite("me1.png"),
-          Sprite("me2.png"),
-        ],
-        stepTime: 0.2,
-      ),
-    )
-      ..anchor = anchor //设置中心点
-      ..setByPosition(toPosition()); //设置位置
+    //设置player动画
+    _live = Animation.spriteList(
+      [
+        Sprite("me1.png"),
+        Sprite("me2.png"),
+      ],
+      stepTime: 0.2,
+    );
   }
 
   void move(Offset offset) {
@@ -49,16 +43,17 @@ class Player extends PositionComponent with HasGameRef<BiuBiuGame> {
 
   @override
   void render(Canvas c) {
+    prepareCanvas(c);
     if (life >= 0) {
-      _live.render(c);
+      _live.getSprite().render(c, width: width, height: height, overridePaint: overridePaint);
     }
   }
 
   @override
   void update(double t) {
     super.update(t);
-    _live.update(t);
-    _live.x = x;
-    _live.y = y;
+    if (life >= 0) {
+      _live.update(t);
+    }
   }
 }
