652d0f1b5646e8792c1213e4653949a00f13aeb2
DinoAndCat <jokerdj@126.com>
2020-08-11 15:39:41 +0800

    # 添加web支持 # 优化player移动

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flame/flame.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'biu_biu_game.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //设置全屏
-  await Flame.util.fullScreen();
-  //设置屏幕方向只能竖屏显示
-  await Flame.util.setPortraitDownOnly();
+
+  if (!kIsWeb) {
+    //设置全屏
+    await Flame.util.fullScreen();
+    //设置屏幕方向只能竖屏显示
+    await Flame.util.setPortraitDownOnly();
+  }
+
   //获取屏幕size
   final Size size = await Flame.util.initialDimensions();
   //稍后创建BiuBiuGame类
